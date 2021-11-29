90bbf7fa9f3b6c8b4e41351fe9c9ed78b4cb94b3
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2021-11-29 18:10:26 +0100

    implementata lista oggetti in data

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 const app = new Vue({
-    data:{},
+    el: '#root',
+
+    data:{
+        todos:[
+            {
+               text: "Comprare l'acqua",
+               done: true 
+            },
+            {
+                text:'Comprare decorazione Natalizie',
+                done: false 
+             },
+             {
+                text:"Fare l'esercizio Vue di Boolean",
+                done: true 
+             },
+        ],
+    },
     methods:{},
-    
+
 });
