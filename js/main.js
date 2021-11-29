f2c5997c9561b519a8c297dd617a104c5fdb03be
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2021-11-29 21:11:49 +0100

    usato operatore ternario per funzione changeTodo

--- js/main.js
+++ js/main.js
@@ -31,11 +31,7 @@ const app = new Vue({
         },
 
         changeTodo: function(index){
-           if(this.todos[index].done == true){
-            this.todos[index].done = false;
-           }else{
-            this.todos[index].done = true;
-           }
+           this.todos[index].done ? this.todos[index].done = false : this.todos[index].done = true
         },
     }
 
